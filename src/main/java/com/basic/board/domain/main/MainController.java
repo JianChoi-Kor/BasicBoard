3e72b769c4ab3d3576170c3f7320f3dc7afc1fc6
JianChoi-Kor <wldks79@gmail.com>
2022-06-05 14:55:20 +0900

    auth_info

--- src/main/java/com/basic/board/domain/main/MainController.java
+++ src/main/java/com/basic/board/domain/main/MainController.java
@@ -1,20 +1,27 @@
 package com.basic.board.domain.main;
 
-import com.basic.board.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @RequestMapping(value = "/")
 @Controller
 public class MainController {
 
+    private final MainService mainService;
+
     @GetMapping(value = {"main", ""})
     public String main() {
         return "page/board-main";
+    }
+
+    @ResponseBody
+    @GetMapping("auth")
+    public ResponseEntity<?> authInfo() {
+        return mainService.authInfo();
     }
 }
